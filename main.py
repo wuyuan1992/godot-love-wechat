2b88faaae4532cf67e7cccf7e22518cafd6e0df1
yuchenyang1994 <yuchenyang1994@foxmail.com>
2024-12-31 16:21:40 +0800

    Add: pyinstaller

--- main.py
+++ main.py
@@ -2,25 +2,36 @@ from nicegui import ui, app
 from app.layout import layout
 from app.settings import settings
 from app.project_list import project_list
+from pathlib import Path
 from app.project import project
 
-app.add_static_files("/assets", "assets")
+static_folder = Path(__file__).parent / 'assets'
+
+app.add_static_files("/assets", str(static_folder))
+
 
 @ui.page("/")
 def index_page():
     with layout("home"):
         project_list()
 
+
 @ui.page("/settings")
 def settings_page():
     with layout("setting"):
         settings()
+
 
 @ui.page("/projects/{id}")
 def project_page(id: str):
     with layout("project"):
         project(id)
 
-        
 
-ui.run(port=24512, title="Godot Love Wechat", window_size=(1024, 768), native=True)
+ui.run(
+    port=24512,
+    title="Godot Love Wechat",
+    window_size=(1024, 768),
+    native=True,
+    reload=False,
+)
